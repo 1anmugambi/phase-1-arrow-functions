a73a20994fa31f0a161cc5da38181499988cadd1
Ian Mugambi <ian.mugambi1@student.moringaschool.com>
2024-03-30 23:00:01 +0300

    Complete Lab

---
+++ index.js
@@ -0,0 +1,14 @@
+// Arrow function to divide two numbers
+const divide = (num1, num2) => num1 / num2;
+
+// Arrow function to square a number
+const square = num => num * num;
+
+// Arrow function to add two numbers
+const add = (num1, num2) => num1 + num2;
+
+module.exports = {
+  divide, // Exporting the divide function
+  square, // Exporting the square function
+  add, // Exporting the add function
+};
